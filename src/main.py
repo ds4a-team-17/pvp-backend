19c4e263947b32efd066f6dfdc443fcff8dd9898
Luiz Picanço <lpicanco@gmail.com>
2021-07-24 19:37:56 +0100

    Add cors support

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from predictor import Predictor, Prediction
 from config import Settings
 
-
 app = FastAPI(docs_url="/", title="Parcel Volume Prediction")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 settings = Settings()
 predictor = Predictor(settings.model_file, settings.tokenizer_file)
 
